a3979f1ed00f6448e745bd8a4bbcee792f1eabf1
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-09 14:25:24 +0100

    new

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpattern=[
-    path('members/', views.members,name=members),
+urlpatterns = [
+    path('members/', views.members, name='members'),
 ]
+
