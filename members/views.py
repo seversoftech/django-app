9cefa985e36b061a610b298b1ab771f960aee1af
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-10 06:26:53 +0100

    new

--- members/views.py
+++ members/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from .models import Member
 
 def members(request):
-  mymembers = Member.objects.all.values()
+  mymembers = Member.objects.all().values()
   template = loader.get_template('all_members.html')
   context = {
     'mymembers' : mymembers,
