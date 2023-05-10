0c61d0d52c208058a755396f26d6c6d78f9ffb6c
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-10 07:07:54 +0100

    new

--- members/views.py
+++ members/views.py
@@ -22,3 +22,11 @@ def details(request, id):
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
+
+def testing(request):
+  templates = loader.get_template('template.html')
+  context={
+    'fruits':['Apple', 'Banana', 'Cherry'],
+    
+  }
+  return HttpResponse(templates.render(context, request))
