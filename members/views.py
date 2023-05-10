e8405bc6ce2dc8a625bac86d527818b63f32105c
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-10 06:40:20 +0100

    new

--- members/views.py
+++ members/views.py
@@ -9,3 +9,12 @@ def members(request):
     'mymembers' : mymembers,
   }
   return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  templates = loader.get_template('details.html')
+  context = {
+    'mymember':mymember,
+  }
+  return HttpResponse(templates.render(context,request))
