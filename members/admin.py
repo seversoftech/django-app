84a537a229f4f4245b84edac08bad1c087da44a4
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-11 05:58:44 +0100

    new

--- members/admin.py
+++ members/admin.py
@@ -4,6 +4,7 @@ from .models import Member
 # Register your models here.
 class MemberAdmin(admin.ModelAdmin):
     list_display=("firstname", "lastname", "joined_date",)
+    prepopulated_fields = {"slug": ("firstname", "lastname")}
 
 
 admin.site.register(Member, MemberAdmin)
