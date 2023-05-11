2e8b2ac2b8d6c7653272764a5e0b90b93eef7982
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-11 09:10:14 +0100

    new

--- members/views.py
+++ members/views.py
@@ -1,18 +1,15 @@
-from django.http import HttpResponse, HttpResponseRedirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
-from django.db.models import Q
 
 def members(request):
   mymembers = Member.objects.all().values()
   template = loader.get_template('all_members.html')
   context = {
-    'mymembers' : mymembers,
+    'mymembers': mymembers,
   }
   return HttpResponse(template.render(context, request))
-
-
+  
 def details(request, slug):
   mymember = Member.objects.get(slug=slug)
   template = loader.get_template('details.html')
@@ -20,16 +17,14 @@ def details(request, slug):
     'mymember': mymember,
   }
   return HttpResponse(template.render(context, request))
-
+  
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
 
 def testing(request):
-  mydata = Member.objects.values_list('firstname')
-  mydata = Member.objects.filter(Q(firstname='Sever') | Q(firstname = 'Dev' )).values()
   template = loader.get_template('template.html')
   context = {
-    'mymembers': mydata,
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
   }
   return HttpResponse(template.render(context, request))
